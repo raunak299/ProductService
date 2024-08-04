fe7b83a1100d76d2aeada497e8ba0714791a4f7f
raunak299 <iamraunak299@gmail.com>
2024-08-04 22:03:52 +0530

    server config for accessing env variables

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 
-const PORT = 4000;
+const apiRoutes = require("./router");
+
+const { PORT } = require("./config/serverConfig");
 
 const setUpAndStartServer = () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
 
   app.listen(PORT, () => {
     console.log(`server started at ${PORT}`);
